58e06cb751bc353d6c0a00390895dd622affb2b8
wuxx <wujianhua0924@gmail.com>
2022-11-03 14:00:44 +0800

    1.54 inch ST7789 240x240 LCD test pass.  On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   app/lcd.c
    	new file:   app/lcd.h
    	modified:   app/main.c
    	modified:   vendor/User/system_ch32v30x.c

--- app/main.c
+++ app/main.c
@@ -18,7 +18,7 @@ void GPIO_Toggle_init(void)
     GPIO_InitTypeDef GPIO_InitStructure = {0};
 
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
@@ -29,16 +29,18 @@ int main(void)
     int i = 0;
 
 	Delay_Init();
-	USART_Printf_Init(115200);
+	USART_Printf_Init(115200); /* PA9 default */
 	printf("SystemClk:%d\r\n",SystemCoreClock);
 	printf("WCH ^V^\r\n");
 
     GPIO_Toggle_init();
 
+    lcd_init();
+
     while (1)
     {
-        Delay_Ms(50);
-        GPIO_WriteBit(GPIOA, GPIO_Pin_0, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
+        Delay_Ms(250);
+        GPIO_WriteBit(GPIOA, GPIO_Pin_3, (i == 0) ? (i = Bit_SET) : (i = Bit_RESET));
     }
 
 	while(1);
